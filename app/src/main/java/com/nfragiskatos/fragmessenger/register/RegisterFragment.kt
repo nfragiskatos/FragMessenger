7e97a40a0cc95b3f51613d9a38a59d4ab80d2bcf
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-30 08:54:24 -0700

    Initial Commit
    - Added Data Binding dependencies
    
    - Added Navigation component dependencies
      - Created navigation graph
      - Added NavHost to main activity
    
    - Added first fragment (register for account)
      - Created layout file
      - Created ViewModel
      - Setup initial bindings in RegisterFragment
      - Set the nav graph to default to this fragment as the initial screen.

---
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterFragment.kt
@@ -0,0 +1,37 @@
+package com.nfragiskatos.fragmessenger.register
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+import com.nfragiskatos.funmessenger.R
+import com.nfragiskatos.funmessenger.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private val viewModel: RegisterViewModel by lazy {
+        ViewModelProviders.of(this).get(RegisterViewModel::class.java)
+    }
+
+    companion object {
+        fun newInstance() = RegisterFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = FragmentRegisterBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.fragmessenger.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
