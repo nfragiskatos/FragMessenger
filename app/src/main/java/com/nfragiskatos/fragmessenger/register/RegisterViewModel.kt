af89444130d7685a85dbfa6331c5f81f932b2e94
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-31 09:04:25 -0700

    - Added new MutableLiveData fields to ViewModel for registration fragment to store input from the user when registering for an account.

--- app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
@@ -1,7 +1,12 @@
 package com.nfragiskatos.fragmessenger.register
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
 
 class RegisterViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val username = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
 }
