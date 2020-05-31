529cda6b975c3e12c76688a93c792c5abb36fedc
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-31 09:30:37 -0700

    - Added Register button to register fragment - Added new Register string to resource file - Setup simple onClickListener for button that just logs the username + password (will change this later)

--- app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterFragment.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.nfragiskatos.fragmessenger.register
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider
 
 import com.nfragiskatos.funmessenger.R
 import com.nfragiskatos.funmessenger.databinding.FragmentRegisterBinding
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
 
@@ -34,6 +36,14 @@ class RegisterFragment : Fragment() {
 
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_register_register.setOnClickListener {
+            Log.d("RegisterFragment", "Username: ${viewModel.username.value}")
+            Log.d("RegisterFragment", "Password: ${viewModel.password.value}")
+        }
     }
 
 }
