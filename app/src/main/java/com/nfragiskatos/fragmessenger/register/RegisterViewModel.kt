f250d2c628c69792910410680e14eff1a1e4921c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-31 14:19:01 -0700

    - Added MutableLiveData/LiveData pattern for variable to be observed to inform that the app needs to navigate from the register screen to the log in screen. - Added helper functions to be called to trigger and terminate the navigation process.

--- app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
@@ -1,5 +1,6 @@
 package com.nfragiskatos.fragmessenger.register
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
 
@@ -8,5 +9,17 @@ class RegisterViewModel : ViewModel() {
     val username = MutableLiveData<String>()
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
+
+    private val _navigateToLogInScreen = MutableLiveData<Boolean>()
+    val navigateToLogInScreen: LiveData<Boolean>
+        get() = _navigateToLogInScreen
+
+    fun displayLogInScreen() {
+        _navigateToLogInScreen.value = true
+    }
+
+    fun displayLogInScreenComplete() {
+        _navigateToLogInScreen.value = false
+    }
 
 }
