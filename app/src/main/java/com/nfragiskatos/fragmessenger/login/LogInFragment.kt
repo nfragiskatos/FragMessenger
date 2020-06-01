a0d57474e06201b3d6770491137c378988715ee2
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-01 15:53:18 -0700

    - Added an onClickListener to the log in button. Similar pattern to how I implemented the register process.

--- app/src/main/java/com/nfragiskatos/fragmessenger/login/LogInFragment.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/login/LogInFragment.kt
@@ -1,13 +1,19 @@
 package com.nfragiskatos.fragmessenger.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 import com.nfragiskatos.fragmessenger.databinding.FragmentLogInBinding
+import java.lang.Exception
 
 class LogInFragment : Fragment() {
 
@@ -20,14 +26,51 @@ class LogInFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
 
         val binding = FragmentLogInBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
+        binding.buttonLogInLogIn.setOnClickListener {
+            performLogIn()
+        }
+
+//        FirebaseAuth.getInstance().signInWithEmailAndPassword(viewModel)
+
         return binding.root
+    }
+
+    private fun performLogIn() {
+        val email = viewModel.email.value
+        val password = viewModel.password.value
+
+        if (email == null || password == null) {
+            Toast.makeText(context, "Please enter text in email and password.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            onCompletedLogIn(it)
+        }.addOnFailureListener {
+            onFailedLogIn(it)
+        }
+    }
+
+    private fun onCompletedLogIn(result: Task<AuthResult>) {
+        if (!result.isSuccessful) {
+            Log.d("LogInFragment", "Completed with failure: ${result.exception}")
+            return
+        }
+
+        Log.d("LogInFragment",
+                "Successfully logged in user with\nuid: ${result.result?.user?.uid}\nemail: ${result.result?.user?.email}"
+        )
+    }
+
+    private fun onFailedLogIn(result: Exception) {
+        Log.d("LogInFragment", "Failed to log in user: ${result.message}")
     }
 }
