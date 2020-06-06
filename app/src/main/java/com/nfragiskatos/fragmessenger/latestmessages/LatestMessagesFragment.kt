1530fcab63e2845088e320114602e99ed7b920e5
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-06 11:32:37 -0700

    - Created options menu on latest messages screen. - Logout option implemented.

--- app/src/main/java/com/nfragiskatos/fragmessenger/latestmessages/LatestMessagesFragment.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/latestmessages/LatestMessagesFragment.kt
@@ -3,10 +3,8 @@ package com.nfragiskatos.fragmessenger.latestmessages
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -54,12 +52,28 @@ class LatestMessagesFragment : Fragment() {
             }
         })
 
-        binding.buttonTestLatestMessages.setOnClickListener {
-            Log.d(TAG, "Signing out...")
-            Firebase.auth.signOut()
-        }
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_new_message -> {
+                Log.d(TAG, "Creating new message...")
+            }
+            R.id.menu_sign_out -> {
+                Log.d(TAG, "Signing out...")
+                Firebase.auth.signOut()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
