3843059418c333c7ec2d6820ec394e2686a7354d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-06 15:48:12 -0700

    - Created a main view model to be observed by the main activity, and to be updated by the fragments (Allow fragments to update the title in the actionbar)

---
+++ app/src/main/java/com/nfragiskatos/fragmessenger/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.nfragiskatos.fragmessenger
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _actionBarTitle = MutableLiveData<String>()
+    val actionBarTitle: LiveData<String>
+        get() = _actionBarTitle
+
+    fun updateActionBarTitle(title: String) = _actionBarTitle.postValue(title)
+}
