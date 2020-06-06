f5c7c31a79770e3fd50fcbe5d397e92d7e1011e6
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-06 08:44:35 -0700

    - Implemented a new route from the latest messages screen to the register screen. It's activated by observing the auth state in firebase to look for any user changes.

--- app/src/main/java/com/nfragiskatos/fragmessenger/latestmessages/LatestMessagesFragment.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/latestmessages/LatestMessagesFragment.kt
@@ -2,15 +2,24 @@ package com.nfragiskatos.fragmessenger.latestmessages
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.nfragiskatos.fragmessenger.R
 import com.nfragiskatos.fragmessenger.databinding.FragmentLatestMessagesBinding
 
 class LatestMessagesFragment : Fragment() {
+
+    private val TAG = "LatestMessagesFragment"
 
     private val viewModel: LatestMessagesViewModel by lazy {
         ViewModelProvider(this).get(LatestMessagesViewModel::class.java)
@@ -26,12 +35,31 @@ class LatestMessagesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentLatestMessagesBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        return inflater.inflate(R.layout.fragment_latest_messages, container, false)
+        Firebase.auth.addAuthStateListener {
+            if (it.currentUser == null) {
+                viewModel.displayRegisterScreen()
+            } else {
+                Log.d(TAG, "${it.currentUser?.email} is already signed in")
+            }
+        }
+
+        viewModel.navigateToRegisterScreen.observe(viewLifecycleOwner, Observer { navigate ->
+            if (navigate) {
+                findNavController().navigate(LatestMessagesFragmentDirections.actionLatestMessagesFragmentToRegisterFragment())
+                viewModel.displayRegisterScreenComplete()
+            }
+        })
+
+        binding.buttonTestLatestMessages.setOnClickListener {
+            Log.d(TAG, "Signing out...")
+            Firebase.auth.signOut()
+        }
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/nfragiskatos/fragmessenger/latestmessages/LatestMessagesViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/latestmessages/LatestMessagesViewModel.kt
@@ -1,7 +1,20 @@
 package com.nfragiskatos.fragmessenger.latestmessages
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LatestMessagesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _navigateToRegisterScreen = MutableLiveData<Boolean>()
+    val navigateToRegisterScreen: LiveData<Boolean>
+        get() = _navigateToRegisterScreen
+
+    fun displayRegisterScreen() {
+        _navigateToRegisterScreen.value = true
+    }
+
+    fun displayRegisterScreenComplete() {
+        _navigateToRegisterScreen.value = false
+    }
 }
