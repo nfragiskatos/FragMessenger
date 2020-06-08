a681fc4ebe43e9abb8cd400a2fb13a46de991516
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-07 17:08:08 -0700

    - Moved all the registration logic into the ViewModel. Will probably have to move it again into a repository at some point. - Created a new log message and notification variable to observe for the fragment to print or toast any messages.

--- app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterFragment.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterFragment.kt
@@ -2,9 +2,7 @@ package com.nfragiskatos.fragmessenger.register
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.ImageDecoder
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -17,20 +15,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import com.nfragiskatos.fragmessenger.databinding.FragmentRegisterBinding
-import java.util.*
 
 class RegisterFragment : Fragment() {
 
     private val TAG = "RegisterFragment"
-
-    private var selectedPhotoUri: Uri? = null
 
     private val viewModel: RegisterViewModel by lazy {
         ViewModelProvider(this).get(RegisterViewModel::class.java)
@@ -50,9 +39,6 @@ class RegisterFragment : Fragment() {
         binding = FragmentRegisterBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
-        binding.textAlreadyHaveAccountRegister.setOnClickListener {
-            viewModel.displayLogInScreen()
-        }
 
         viewModel.navigateToLogInScreen.observe(viewLifecycleOwner, Observer { navigate ->
             if (navigate) {
@@ -61,10 +47,6 @@ class RegisterFragment : Fragment() {
                 viewModel.displayLogInScreenComplete()
             }
         })
-
-        binding.buttonRegisterRegister.setOnClickListener {
-            performRegistration()
-        }
 
         binding.buttonSelectPhotoRegister.setOnClickListener {
             Log.d(TAG, "Try and show photo selector")
@@ -81,55 +63,18 @@ class RegisterFragment : Fragment() {
             }
         })
 
+        viewModel.logMessage.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, it)
+        })
+
+        viewModel.notification.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT)
+                .show()
+        })
+
 
         setHasOptionsMenu(true)
         return binding.root
-    }
-
-    private fun performRegistration() {
-        val email = viewModel.email.value
-        val password = viewModel.password.value
-
-        if (email == null || password == null) {
-            Toast.makeText(context, "Please enter text in email and password.", Toast.LENGTH_SHORT)
-                .show()
-            return
-        }
-
-        Firebase.auth.createUserWithEmailAndPassword(email, password)
-            .addOnSuccessListener {
-                onCompletedRegistration(it)
-            }
-            .addOnFailureListener {
-                onFailedRegistration(it)
-            }
-    }
-
-    private fun onCompletedRegistration(result: AuthResult) {
-        Log.d(
-            TAG,
-            "Successfully created user with\nuid: ${result.user?.uid}\nemail: ${result.user?.email}"
-        )
-        uploadImageToFirebaseStorage()
-    }
-
-    private fun onCompletedRegistration(result: Task<AuthResult>) {
-        if (!result.isSuccessful) {
-            Log.d(TAG, "Completed with failure: ${result.exception}")
-            return
-        }
-
-        Log.d(
-            TAG,
-            "Successfully created user with\nuid: ${result.result?.user?.uid}\nemail: ${result.result?.user?.email}"
-        )
-        uploadImageToFirebaseStorage()
-    }
-
-    private fun onFailedRegistration(result: Exception) {
-        Toast.makeText(context, "Failed to create user: ${result.message}", Toast.LENGTH_SHORT)
-            .show()
-        Log.d(TAG, "Failed to create user: ${result.message}")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -137,12 +82,12 @@ class RegisterFragment : Fragment() {
 
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
             Log.d(TAG, "Photo was selected")
-            selectedPhotoUri = data.data
+            viewModel.selectedPhotoUri.value = data.data
 
             val bitmap = when {
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> {
                     activity?.contentResolver?.let { contentResolver ->
-                        selectedPhotoUri?.let { photoUri ->
+                        viewModel.selectedPhotoUri.value?.let { photoUri ->
                             val source = ImageDecoder.createSource(
                                 contentResolver,
                                 photoUri
@@ -153,7 +98,7 @@ class RegisterFragment : Fragment() {
                 }
                 else -> MediaStore.Images.Media.getBitmap(
                     activity?.contentResolver,
-                    selectedPhotoUri
+                    viewModel.selectedPhotoUri.value
                 )
             }
 
@@ -162,43 +107,4 @@ class RegisterFragment : Fragment() {
             binding.buttonSelectPhotoRegister.alpha = 0f
         }
     }
-
-    private fun uploadImageToFirebaseStorage() {
-
-        if (selectedPhotoUri == null) {
-            return
-        }
-
-        var filename = UUID.randomUUID().toString()
-        val ref = Firebase.storage.getReference("/images/$filename")
-
-        ref.putFile(selectedPhotoUri!!)
-            .addOnSuccessListener { it ->
-                Log.d(TAG, "Successfully uploaded image: ${it.metadata?.path}")
-
-                ref.downloadUrl.addOnSuccessListener {
-
-                    Log.d(TAG, "File Location: $it")
-
-                    saveUserToFirebaseDatabase(it.toString())
-                }
-            }
-    }
-
-    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
-        val uid = Firebase.auth.uid ?: ""
-        val ref = Firebase.database.getReference("/users/$uid")
-
-        val user = User(uid, viewModel.username.value ?: "", profileImageUrl)
-        ref.setValue(user)
-            .addOnSuccessListener {
-                Log.d(TAG, "Finally saved user to Firebase database")
-                viewModel.displayLatestMessagesScreen()
-            }
-            .addOnFailureListener {
-                Log.d(TAG, "Failed: $it")
-            }
-    }
 }
-
-class User(val uid: String, val username: String, val profileImageUrl: String)
--- app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/register/RegisterViewModel.kt
@@ -1,14 +1,30 @@
 package com.nfragiskatos.fragmessenger.register
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.*
 
 class RegisterViewModel : ViewModel() {
 
     val username = MutableLiveData<String>()
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
+    val selectedPhotoUri = MutableLiveData<Uri>()
+
+    private val _notification = MutableLiveData<String>()
+    val notification: LiveData<String>
+        get() = _notification
+
+    private val _logMessage = MutableLiveData<String>()
+    val logMessage: LiveData<String>
+        get() = _logMessage
 
     private val _navigateToLogInScreen = MutableLiveData<Boolean>()
     val navigateToLogInScreen: LiveData<Boolean>
@@ -35,4 +51,70 @@ class RegisterViewModel : ViewModel() {
         _navigateToLatestMessagesScreen.value = false
     }
 
+    fun performRegistration() {
+        if (email.value == null || password.value == null) {
+            _notification.value = "Please enter text in email and password."
+            return
+        }
+
+        email.value?.let { email ->
+            password.value?.let { password ->
+                Firebase.auth.createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener {
+                        onCompletedRegistration(it)
+                    }
+                    .addOnFailureListener {
+                        onFailedRegistration(it)
+                    }
+            }
+        }
+    }
+
+    private fun onCompletedRegistration(result: AuthResult) {
+        _logMessage.value =
+            "Successfully created user with\nuid: ${result.user?.uid}\nemail: ${result.user?.email}"
+        uploadImageToFirebaseStorage()
+    }
+
+    private fun onFailedRegistration(result: Exception) {
+        _logMessage.value = "Failed to create user: ${result.message}"
+        _notification.value = "Failed to create user: ${result.message}"
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+
+        if (selectedPhotoUri.value == null) {
+            return
+        }
+
+        var filename = UUID.randomUUID().toString()
+        val ref = Firebase.storage.getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri.value!!)
+            .addOnSuccessListener { it ->
+                _logMessage.value = "Successfully uploaded image: ${it.metadata?.path}"
+
+                ref.downloadUrl.addOnSuccessListener {
+                    _logMessage.value = "File Location: $it"
+                    saveUserToFirebaseDatabase(it.toString())
+                }
+            }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = Firebase.auth.uid ?: ""
+        val ref = Firebase.database.getReference("/users/$uid")
+
+        val user = User(uid, username.value ?: "", profileImageUrl)
+        ref.setValue(user)
+            .addOnSuccessListener {
+                _logMessage.value = "Finally saved user to Firebase database"
+                displayLatestMessagesScreen()
+            }
+            .addOnFailureListener {
+                _logMessage.value = "Failed: $it"
+            }
+    }
 }
+
+class User(val uid: String, val username: String, val profileImageUrl: String)
