fd6f9c7468547111e0d9ea01e1d8afc757d8e58a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-11 20:01:01 -0700

    - Created new binding adapter to bind a list of ChatMessageItem. - For clarity, I changed the other binding adapter for User to have a more unique name

--- app/src/main/java/com/nfragiskatos/fragmessenger/BindingAdapters.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/BindingAdapters.kt
@@ -2,12 +2,23 @@ package com.nfragiskatos.fragmessenger
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.nfragiskatos.fragmessenger.newmessage.UserListAdapter
+import com.nfragiskatos.fragmessenger.chatlog.MessageListAdapter
+import com.nfragiskatos.fragmessenger.domain.ChatMessageItem
 import com.nfragiskatos.fragmessenger.domain.User
+import com.nfragiskatos.fragmessenger.newmessage.UserListAdapter
 
 
-@BindingAdapter("listData")
+@BindingAdapter("userListData")
 fun bindUserRecyclerView(recyclerView: RecyclerView, data: List<User>?) {
     val adapter = recyclerView.adapter as UserListAdapter
+    adapter.submitList(data)
+}
+
+@BindingAdapter("messageListData")
+fun bindChatMessageRecyclerView(
+    recyclerView: RecyclerView,
+    data: List<ChatMessageItem.FromMessage>?
+) {
+    val adapter = recyclerView.adapter as MessageListAdapter
     adapter.submitList(data)
 }
