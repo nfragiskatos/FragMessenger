d355346e140399c023c7ad13475c3359ea9d8e58
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-23 19:42:44 -0700

    - Extended the repository with new functionality   - registering a user   - Uploading an image to Firebase Storage   - Create a User in the Firebase Database

--- app/src/main/java/com/nfragiskatos/fragmessenger/repository/FirebaseRepository.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/repository/FirebaseRepository.kt
@@ -1,17 +1,48 @@
 package com.nfragiskatos.fragmessenger.repository
 
+import android.net.Uri
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.nfragiskatos.fragmessenger.domain.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class FirebaseRepository {
 
-    suspend fun performLogIn(email: String, password: String): Boolean {
+    suspend fun performLogIn(email: String, password: String): AuthResult? {
         return withContext(Dispatchers.IO) {
-            Firebase.auth.signInWithEmailAndPassword(email, password).await()
-            return@withContext Firebase.auth.currentUser != null
+            return@withContext Firebase.auth.signInWithEmailAndPassword(email, password).await()
         }
+    }
+
+    suspend fun performRegistration(email: String, password: String): AuthResult? {
+        return withContext(Dispatchers.IO) {
+            return@withContext Firebase.auth.createUserWithEmailAndPassword(email, password).await()
+        }
+    }
+
+    suspend fun uploadImageToStorage(uri: Uri, fileName: String, nodePath: String): Uri? {
+        return withContext(Dispatchers.IO) {
+            val path = validatePath(nodePath)
+            val ref = Firebase.storage.getReference(path + fileName)
+            ref.putFile(uri).await()
+            return@withContext ref.downloadUrl.await()
+        }
+    }
+
+    suspend fun saveUserToDatabase(user: User, uid: String, nodePath: String) {
+        withContext(Dispatchers.IO) {
+            val path = validatePath(nodePath)
+            val ref = Firebase.database.getReference(path + uid)
+            ref.setValue(user).await()
+        }
+    }
+
+    private fun validatePath(nodePath: String) : String {
+        return if (nodePath[nodePath.lastIndex] == '/') nodePath else "$nodePath/"
     }
 }
