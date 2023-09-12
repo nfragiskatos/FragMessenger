dba4de61995ddf69f64f762a1b0465d5fce9c94a
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-09-11 19:56:42 -0700

    - removed data binding onClick from the log in button and instead handling it in the fragment so I can auto close the soft input keyboard - refactored - removed dead code

--- app/src/main/java/com/nfragiskatos/fragmessenger/login/LogInFragment.kt
+++ app/src/main/java/com/nfragiskatos/fragmessenger/login/LogInFragment.kt
@@ -1,41 +1,37 @@
 package com.nfragiskatos.fragmessenger.login
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.nfragiskatos.fragmessenger.R
 
 import com.nfragiskatos.fragmessenger.databinding.FragmentLogInBinding
-import java.lang.Exception
+
 
 private const val TAG = "LogInFragment"
 
 class LogInFragment : Fragment() {
 
+    private lateinit var binding: FragmentLogInBinding
+
     private val viewModel: LogInViewModel by lazy {
         ViewModelProvider(this).get(LogInViewModel::class.java)
-    }
-
-    companion object {
-        fun newInstance() = LogInFragment()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        val binding = FragmentLogInBinding.inflate(inflater)
+        binding = FragmentLogInBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
@@ -47,6 +43,17 @@ class LogInFragment : Fragment() {
             }
         })
 
+        binding.buttonLogInLogIn.setOnClickListener {
+            val imm: InputMethodManager = requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+            viewModel.performLogIn()
+        }
+
+        initObservers()
+        return binding.root
+    }
+
+    private fun initObservers() {
         viewModel.logMessage.observe(viewLifecycleOwner, Observer {
             Log.d(TAG, it)
         })
@@ -59,24 +66,18 @@ class LogInFragment : Fragment() {
         viewModel.status.observe(viewLifecycleOwner, Observer {status ->
             when (status) {
                 LogInStatus.LOADING -> {
-//                    binding.buttonLogInLogIn.visibility = View.GONE
-//                    binding.textEmailLogIn.visibility = View.GONE
-//                    binding.textPasswordLogIn.visibility = View.GONE
-                    binding.textPasswordLogIn.isEnabled = false
-                    binding.textEmailLogIn.isEnabled = false
-                    binding.buttonLogInLogIn.isEnabled = false
+                    setLoginEnabled(false)
                 }
-                LogInStatus.DONE, LogInStatus.ERROR -> {
-//                    binding.buttonLogInLogIn.visibility = View.VISIBLE
-//                    binding.textEmailLogIn.visibility = View.VISIBLE
-//                    binding.textPasswordLogIn.visibility = View.VISIBLE
-                    binding.buttonLogInLogIn.isEnabled = true
-                    binding.textEmailLogIn.isEnabled = true
-                    binding.textPasswordLogIn.isEnabled = true
+                else -> {
+                    setLoginEnabled(true)
                 }
             }
         })
+    }
 
-        return binding.root
+    private fun setLoginEnabled(isEnabled: Boolean) {
+        binding.textPasswordLogIn.isEnabled = isEnabled
+        binding.textEmailLogIn.isEnabled = isEnabled
+        binding.buttonLogInLogIn.isEnabled = isEnabled
     }
 }
